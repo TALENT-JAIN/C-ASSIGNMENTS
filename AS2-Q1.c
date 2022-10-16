05c892da91792433b40f5ec36a41d82eab8ea9fc
TALENT <talent2004jain@gmail.com>
2022-10-16 13:09:56 +0530

    assigment 2

---
+++ AS2-Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+//Q1
+int a;
+//int 0a;//invalid
+int ZeroNumber;
+int _first;
+//int 009;//invalid
+//int year@22;//invalid
+int sept21;
+int YahooGoogle;
+//int else;//invalid
+//int int;//invalid
+    return 0;
+}

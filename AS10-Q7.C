e1bd5adc6954dcab105d55d9f8ce8732c484a8be
TALENT <talent2004jain@gmail.com>
2022-10-16 23:06:01 +0530

    initial commit

---
+++ AS10-Q7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int i;
+float f;
+printf("testing the various arithematic operations\n");
+i=2.0/5;
+f=2.0/5;
+printf("value of interger i is%d\n",i);
+
+printf("value of float f is%f\n",f);
+    return 0;
+}
